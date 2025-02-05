20e0c8c51b7c7048a4aef765a5d1d65daa09cf8e
Temiloluwa <afolabitemiloluwa@gmail.com>
2025-02-05 12:58:35 +0100

    First commit with just the folders and the snippet

---
+++ src/test/java/com/hng/number_api/NumberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hng.number_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
